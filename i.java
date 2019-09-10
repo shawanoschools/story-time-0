44c86046fe09691d5f81eca35a9f1dcc4cc07236
I-give-up-on-GitHub-already <54900193+I-give-up-on-GitHub-already@users.noreply.github.com>
2019-09-10 09:06:00 -0500

    Add files via upload

--- i.java
+++ i.java
@@ -1,5 +1,10 @@
 public class i{
     public static void main(String[] args){
+     System.out.println("We told him to stay home, but he was follish");
+     System.out.println("He told us to be quiet as he walked down the street");
+     System.out.println("The street was quiet and dark as can be when he began to fall");
+     System.out.println("his breathing was gone in only a fear of shock");
+     System.out.println("The only thing we heard was the sound of a plop.");
      System.out.println("010000010110111001100100");
      System.out.println("0110100001100101");
      System.out.println("011001110110111101110100");
