d12c08bcb17e4dd2a547a5a39b56f069809de35f
WestonM <54899670+WestonM21@users.noreply.github.com>
2019-09-10 09:06:41 -0500

    Add files via upload

---
+++ Story.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Story here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Story
+{
+    public static void main(String[] args){
+        System.out.println("?");
+        System.out.println("Where am I?");
+        System.out.println("Hello?");
+        System.out.println("Huh?");
+        System.out.println("...");
+        System.out.println("Hello?????");
+        System.out.println("Hello");
+        System.out.println("Who said that?");
+        System.out.println("Am I Dead?");
+        System.out.println("Yes");
+        System.out.println("oh");
+    }
+}
+    
