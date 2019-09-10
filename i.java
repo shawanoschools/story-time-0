e0f246561c820a0e8e624829e9ce414e844cd830
I-give-up-on-GitHub-already <54900193+I-give-up-on-GitHub-already@users.noreply.github.com>
2019-09-10 08:50:41 -0500

    Add files via upload

---
+++ i.java
@@ -0,0 +1,11 @@
+public class i{
+    public static void main(String[] args){
+     System.out.println("010000010110111001100100");
+     System.out.println("0110100001100101");
+     System.out.println("011001110110111101110100");
+     System.out.println("011010000110100101110100");
+     System.out.println("0110001001111001");
+     System.out.println("01000001");
+     System.out.println("01100010011101010111001100101110");
+    }
+}
