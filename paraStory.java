e4173fd5fd06943fbf61d1ea7bc61658e62a96b8
Brandonpeters623 <54899467+Brandonpeters623@users.noreply.github.com>
2019-09-10 08:56:42 -0500

    Add files via upload
    Brandon

---
+++ paraStory.java
@@ -0,0 +1,23 @@
+
+/**
+ * My Paragraph story
+ *this is the story of a paragraph
+ * @author (Brandon Peters)
+ * @version (Sept 10 2019)
+ */
+public class paraStory
+{
+    
+    
+        public static void main(String[]args){
+        System.out.println("We told him to stay home, but he was follish");
+        System.out.println("He told us to be quiet as he walked down the street");
+        System.out.println("The street was quiet and dark as can be when he began to fall");
+        System.out.println("his breathing was gone in only a fear of shock");
+        System.out.println("The only thing we heard was the sound of a plop.");
+        
+        
+        
+       
+    }
+}
